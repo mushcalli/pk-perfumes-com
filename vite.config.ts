b606d5f8589446b891192be32947e7170bf49f1a
mushcalli <mushcalli@gmail.com>
2026-09-04 21:46:05 -0700

    deployment testing 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
+  base: "/pk-perfumes-com/",
   resolve: {
     tsconfigPaths: true,
   },
